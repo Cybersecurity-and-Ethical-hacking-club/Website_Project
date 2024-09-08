322a4a254d80a767ba1fb60f4c39277c940d70bc
Apple <apple@MacBook-pro.local>
2024-09-08 17:41:33 +0100

    first push react

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Ceci indique à Tailwind d'appliquer les styles aux fichiers dans 'src'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
